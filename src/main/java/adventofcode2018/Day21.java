7c77f284255619478ac358c666a257810243dfc0
Mark Wright <mwright@cygnacom.com>
2019-02-04 11:23:30 -0500

    Solved day 21

--- src/main/java/adventofcode2018/Day21.java
+++ src/main/java/adventofcode2018/Day21.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,6 +14,41 @@ import adventofcode2018.Day19.Instruction;
 import adventofcode2018.Day19.WristDeviceProgram;
 
 public class Day21 {
+
+    static public class ExitOnRepeat extends Instruction {
+        int lastR2Seen = 0;
+        LinkedHashSet<Integer> r2Seen = new LinkedHashSet<Integer>();
+
+        public ExitOnRepeat(Instruction a) {
+            super(a.opcodeName, a.inputA, a.inputB, a.outputC);
+        }
+
+        @Override
+        public void apply(int[] r) {
+            super.apply(r);
+            if (r2Seen.contains(r[2])) {
+                r[5] = 1000;
+                System.out.println("Part two:" + lastR2Seen);
+            } else {
+                r2Seen.add(r[2]);
+                lastR2Seen = r[2];
+            }
+        }
+    }
+
+    static public class DivideBy256 extends Instruction {
+
+        public DivideBy256() {
+            super("d256", 1, 0, 3);
+        }
+
+        @Override
+        public void apply(int[] r) {
+            r[4] = r[1] / 256;
+            r[5] += 8;
+        }
+    }
+
     static class PrintAndExitInstruction extends Instruction {
 
         public PrintAndExitInstruction(Instruction a) {
@@ -29,62 +65,33 @@ public class Day21 {
 
     }
 
-    static class PrintAndContinueInstruction extends Instruction {
-        int lastAnswer = 0;
-
-        public static String padLeft(String s, int n) {
-            return String.format("%" + n + "s", s);
-        }
-
-        int count = 0;
-
-        public PrintAndContinueInstruction(Instruction a) {
-            super(a.opcodeName, a.inputA, a.inputB, a.outputC);
-        }
-
-        @Override
-        public void apply(int[] r) {
-            super.apply(r);
-            System.out.println(
-                    "Part two:    " + padLeft(Integer.toBinaryString(r[2]), 30));
-            if (lastAnswer != 0) {
-                System.out.println("part two (*):" + r[2] / lastAnswer);
-                System.out.println("part two (*):" + padLeft(
-                        Integer.toBinaryString(r[2] / lastAnswer), 30));
-                System.out.println("part two (-):" + (r[2] - lastAnswer));
-                System.out.println("part two (-):" + padLeft(
-                        Integer.toBinaryString(r[2] - lastAnswer), 30));
-            }
-            lastAnswer = r[2];
-            if (count++ >= 1000)
-                r[5] = 1000;
-        }
-
-    }
-
     public static void main(String[] args) throws IOException {
         int finalComparisonInstructionNumber = 28;
+        int divideInstructionNumber = 17;
+        Map<String, OpCode> opCodes = Day16.setupOpCodes();
 
         List<String> lines = Files.readAllLines(Paths.get("data", "day21.txt"));
-
         String ipLine = lines.get(0);
         int instructionPointer = Integer.valueOf(ipLine.split("\\s+")[1]);
+
         List<Instruction> instructions = lines.stream().skip(1)
                 .map(Instruction::parse).collect(Collectors.toList());
+        instructions.set(divideInstructionNumber, new DivideBy256());
         instructions.set(finalComparisonInstructionNumber,
                 new PrintAndExitInstruction(
                         instructions.get(finalComparisonInstructionNumber)));
-        Map<String, OpCode> opCodes = Day16.setupOpCodes();
-
         WristDeviceProgram program = new WristDeviceProgram(opCodes,
                 instructions, instructionPointer);
         program.runWithRegisters(new int[] { 0, 0, 0, 0, 0, 0 });
 
-        instructions.set(finalComparisonInstructionNumber,
-                new PrintAndContinueInstruction(
-                        instructions.get(finalComparisonInstructionNumber)));
-        WristDeviceProgram program2 = new WristDeviceProgram(opCodes,
-                instructions, instructionPointer);
+        // part two
+        List<Instruction> instructions2 = lines.stream().skip(1)
+                .map(Instruction::parse).collect(Collectors.toList());
+        instructions2.set(divideInstructionNumber, new DivideBy256());
+        instructions2.set(finalComparisonInstructionNumber, new ExitOnRepeat(
+                instructions2.get(finalComparisonInstructionNumber)));
+        var program2 = new WristDeviceProgram(opCodes, instructions2,
+                instructionPointer);
         program2.runWithRegisters(new int[] { 0, 0, 0, 0, 0, 0 });
     }
 }
