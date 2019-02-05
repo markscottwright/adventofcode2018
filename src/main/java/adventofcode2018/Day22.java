e2e1965e5a528b20dd39ab39a903907be4b7de2f
Mark Wright <markscottwright@gmail.com>
2019-02-05 07:56:12 -0500

    Day22 part two - adding bfs algorithm

--- src/main/java/adventofcode2018/Day22.java
+++ src/main/java/adventofcode2018/Day22.java
@@ -1,6 +1,10 @@
 package adventofcode2018;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.PriorityQueue;
+
+import org.javatuples.Pair;
 
 public class Day22 {
     enum RegionType {
@@ -16,6 +20,64 @@ public class Day22 {
             return riskLevel;
         }
     };
+
+    enum Equipment {
+        TORCH, CLIMBING_GEAR, NOTHING;
+        
+        Equipment change(Step move) {
+            if (move.equals(Step.EQUIP_CLIMBING_GEAR))
+                return CLIMBING_GEAR;
+            else if (move.equals(Step.EQUIP_TORCH))
+                return TORCH;
+            else if (move.equals(Step.EQUIP_NOTHING))
+                return NOTHING;
+            else
+                return this;
+        }
+    }
+
+    enum Step {
+        MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT, EQUIP_TORCH,
+        EQUIP_CLIMBING_GEAR, EQUIP_NOTHING;
+
+        static int weight(ArrayList<Step> steps) {
+            return steps.stream().mapToInt(s -> Step.weight(s)).sum();
+        }
+
+        static int weight(Step s) {
+            switch (s) {
+            case MOVE_LEFT:
+            case MOVE_DOWN:
+            case MOVE_UP:
+            case MOVE_RIGHT:
+                return 1;
+            default:
+                return 7;
+            }
+        }
+    }
+    
+    static class Point extends adventofcode2018.Point {
+
+        public Point(int x, int y) {
+            super(x, y);
+        }
+        
+        private Point doMove(Step move) {
+            switch (move) {
+            case MOVE_DOWN:
+                return new Point(x, y + 1);
+            case MOVE_LEFT:
+                return new Point(x - 1, y);
+            case MOVE_RIGHT:
+                new Point(x + 1, y);
+            case MOVE_UP:
+                return new Point(x, y - 1);
+            default:
+                return this;
+            }
+        }
+    }
 
     static class Cave {
         final private HashMap<Point, Integer> geologicIndices;
@@ -73,26 +135,107 @@ public class Day22 {
             return geologicIndices.keySet().stream().map(p -> getType(p.x, p.y))
                     .mapToInt(RegionType::getRiskLevel).sum();
         }
+
+        Path findPath() {
+
+            HashMap<Pair<Point, Equipment>, Path> shortedPathAtPoint = new HashMap<>();
+            Path emptyPath = new Path();
+            shortedPathAtPoint.put(Pair.with(emptyPath.getFinalPoint(), emptyPath.getEquipment()),
+                    emptyPath);
+
+            PriorityQueue<Day22.Path> toConsider = new PriorityQueue<>(comparator) 
+            // found a path
+            if (pos.equals(new Point(maxX, maxY))
+                    && equipped.equals(Equipment.TORCH))
+                return stepsSoFar;
+
+            // pick next move
+            for (Step move : Step.values()) {
+                Point nextPos = doMove(move, pos);
+                Equipment nextEquipment = changeEquipment(move, equipped);
+
+                if (nextPos.x < 0 || nextPos.y < 0)
+                    continue;
+                if (nextPos.equals(pos) && nextEquipment.equals(equipped))
+                    continue;
+                if (!equipmentAllowed(nextEquipment, pos)
+                        || !equipmentAllowed(nextEquipment, nextPos))
+                    continue;
+            }
+        }
+
+        
+
+        
+
+        private boolean equipmentAllowed(Equipment value1, Point position) {
+            if (getType(position.x, position.y) == RegionType.narrow) {
+                return value1.equals(Equipment.TORCH)
+                        || value1.equals(Equipment.NOTHING);
+            } else if (getType(position.x, position.y) == RegionType.rocky) {
+                return value1.equals(Equipment.TORCH)
+                        || value1.equals(Equipment.CLIMBING_GEAR);
+            } else {
+                return value1.equals(Equipment.CLIMBING_GEAR)
+                        || value1.equals(Equipment.NOTHING);
+            }
+        }
+    }
+
+    static class Path {
+        final ArrayList<Step> steps;
+        final int weight;
+    
+        Path() {
+            steps = new ArrayList<>();
+            weight = 0;
+        }
+    
+        public Object getEquipment() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public Object getFinalPoint() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        private Path(ArrayList<Step> steps, int weight) {
+            this.steps = steps;
+            this.weight = weight;
+        }
+    
+        Path addStep(Step step) {
+            var newSteps = new ArrayList<>(steps);
+            newSteps.add(step);
+            int newWeight = weight + Step.weight(step);
+            return new Path(newSteps, newWeight);
+        }
     }
 
     public static void main(String[] args) {
         var cave = new Cave(14, 709, 6084);
         System.out.println("Part one: " + cave.totalRiskLevel());
-        
+
         // part two:
-        // Breadth-first search?  Order next choice by weight of move into space, nearness of
+        // Breadth-first search? Order next choice by weight of move into space,
+        // nearness of
         // map<tuple<Point,Equipped>, Route> metadata;
         // route = findRoute(cave, point, equipped, metadata)
         // if point == endPoint && equipment == torch)
-        //      return metadata.get(point, equipment)
-        // for step in (moveUp, moveRight, moveDown, moveLeft, equipTorch, equipClimbingGear, equipNothing)
-        //      potentialPosition = move.getPosition(p)
-        //      potentialEquipment = move.getEquipment()
-        //      if isAllowed(p, potentialEquipment) && isAllowed(potentialPosition) && isAllowed(potentialPosition, potentialEquipment) 
-        //          moves.add(step)
+        // return metadata.get(point, equipment)
+        // for step in (moveUp, moveRight, moveDown, moveLeft, equipTorch,
+        // equipClimbingGear, equipNothing)
+        // potentialPosition = move.getPosition(p)
+        // potentialEquipment = move.getEquipment()
+        // if isAllowed(p, potentialEquipment) && isAllowed(potentialPosition)
+        // && isAllowed(potentialPosition, potentialEquipment)
+        // moves.add(step)
         // sortMoves
         // for move in moves:
-        //      if betterMove(): metadata(potentialPosition, potentialEquipment) = metadata(p, equipped) + move
-        //     findPath(cave, move.getPoint(p), move.getEquipment(), metadata)
+        // if betterMove(): metadata(potentialPosition, potentialEquipment) =
+        // metadata(p, equipped) + move
+        // findPath(cave, move.getPoint(p), move.getEquipment(), metadata)
     }
 }
